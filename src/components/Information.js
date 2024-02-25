bd62dc17bc8775cb71649375c1889bbd567ee580
Aleksey Beketov <alex311271@gmail.com>
2024-02-25 20:10:43 +0300

    initial commit

--- src/components/Information.js
+++ src/components/Information.js
@@ -1,7 +1,9 @@
 import { STATUS, PLAYER_ACTION } from '../constants';
 import { InformationLayout } from './InformationLayout';
+import { store } from '../redux/store';
 
 export const Information = ({ currentPlayer, status }) => {
+	// const { currentPlayer, status } = store.getState();
 	const playerAction = PLAYER_ACTION[status];
 	let textInformation =
 		status === STATUS.DRAW ? 'Ничья' : `${playerAction}: ${currentPlayer}`;
